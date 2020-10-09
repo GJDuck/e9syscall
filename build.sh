0d163d93ac343d340a65df03288036d328a0fcbc
GJDuck <gregory@comp.nus.edu.sg>
2020-10-09 14:53:44 +0800

    Add missing build.sh script

---
+++ build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Copyright (C) 2020 National University of Singapore
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ -t 1 ]
+then
+    RED="\033[31m"
+    GREEN="\033[32m"
+    YELLOW="\033[33m"
+    BOLD="\033[1m"
+    OFF="\033[0m"
+else
+    RED=
+    GREEN=
+    YELLOW=
+    BOLD=
+    OFF=
+fi
+
+set -e
+
+VERSION=0471829fd65f1d7c4ee5ac01682d84d04765061f
+
+# STEP (1): install e9patch if necessary:
+if [ ! -x ./e9patch ]
+then
+    if [ ! -f e9patch-$VERSION.zip ]
+    then
+        echo -e "${GREEN}$0${OFF}: downloading e9patch-$VERSION.zip..."
+        wget -O e9patch-$VERSION.zip \
+            https://github.com/GJDuck/e9patch/archive/$VERSION.zip
+    fi
+
+    echo -e "${GREEN}$0${OFF}: extracting e9patch-$VERSION.zip..."
+    unzip e9patch-$VERSION.zip
+
+    echo -e "${GREEN}$0${OFF}: building e9patch..."
+    cd e9patch-$VERSION
+    ./build.sh
+    cd ..
+    ln -f -s e9patch-$VERSION/e9patch
+    ln -f -s e9patch-$VERSION/e9tool
+    ln -f -s e9patch-$VERSION/e9compile.sh
+    echo -e "${GREEN}$0${OFF}: e9patch has been built..."
+else
+	echo -e "${GREEN}$0${OFF}: using existing e9patch..."
+fi
+
